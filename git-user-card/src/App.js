5327da1bdd2d60f64a77276119972c528681c0db
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-08-04 16:29:51 -0700

    added axios and fetch data

--- git-user-card/src/App.js
+++ git-user-card/src/App.js
@@ -1,13 +1,51 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import axios from "axios";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Welcome to GitHub User!</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gitdata: [],
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    axios // fetch intial data
+      .get("https://api.github.com/users/bikesh-maharjan")
+
+      .then((res) => {
+        this.setState([
+          {
+            gitdata: res.data,
+          },
+        ]);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+
+    axios
+      .get("https://api.github.com/users/bikesh-maharjan/followers")
+      .then((res) => {
+        this.setState([
+          {
+            users: res.data,
+          },
+        ]);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Welcome to GitHub User!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
